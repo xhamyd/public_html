ceff855aeb62a5765729215d26c565dd01f02f98
David Buzzell <xhamyd@gmail.com>
2020-10-25 01:03:57 -0400

    Pointing Autocommentor to correct script location (#60)
    * init commit
    
    * Making script executable
    
    * Updating with correct token values
    
    * Created auto and manual
    
    * renaming to split up better

--- .github/workflows/manualcomment.sh
+++ .github/workflows/manualcomment.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env bash
 
-if [[ "${COMMENTER}" == "xhamyd" ]]; then
-    echo "Comment was made by me, no need to autocomment!"
-    return
-fi
-
-OUTPUT=$(curl -H "Accept: application/vnd.github.v3+json" \
-         -H "Authorization: token ${OAUTH_TOKEN}" \
-         https://api.github.com/repos/xhamyd/public_html/issues/${ISSUE_NUMBER}/labels)
-echo "Labels on Issue #${ISSUE_NUMBER}: ${OUTPUT}"
-if [[ "$OUTPUT" == *"Autocommented"* ]]; then
-    echo "Already commented before, no need to autocomment now!"
-    return
-fi
-
 curl -X POST -H "Authorization: token ${OAUTH_TOKEN}" \
     https://api.github.com/repos/xhamyd/public_html/issues/${ISSUE_NUMBER}/comments \
     -d "{\"body\":\"Hi @${COMMENTER} , thanks for showing interest in my Personal Website project! Right now, \
@@ -22,7 +8,3 @@ curl -X POST -H "Authorization: token ${OAUTH_TOKEN}" \
                     have a contribution license set up for this project, I encourage you to comment on #33 for \
                     any bugs or suggestions you may have for the project. Thanks again for your interest, and \
                     take care!\"}"
-curl -X PUT -H "Accept: application/vnd.github.v3+json" \
-    -H "Authorization: token ${OAUTH_TOKEN}" \
-    https://api.github.com/repos/xhamyd/public_html/issues/${ISSUE_NUMBER}/labels \
-    -d '{"labels":["Autocommented"]}'
